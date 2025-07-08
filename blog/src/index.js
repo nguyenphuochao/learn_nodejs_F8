b2a92226ea329b3ba000884e7a0e75114436d717
HaoNP <nguyenphuochao123@gmail.com>
2025-07-09 00:18:03 +0700

    MVC : Router & Controller

--- blog/src/index.js
+++ blog/src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const { engine } = require('express-handlebars');
 const morgan = require('morgan');
+const route = require('./routes');
 const app = express();
 const port = 3000;
 
@@ -21,15 +22,8 @@ app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// router
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-app.get('/news', (req, res) => {
-    res.render('news')
-})
-
+// Routes init
+route(app);
 
 // start project [node index.js]
 // start project with nodemon [npm start]
