853792f422384429cc2a45f1dfe528ada75881d7
HaoNP <nguyenphuochao123@gmail.com>
2025-07-13 15:07:55 +0700

    "Select all" with checkbox

--- blog/src/routes/course.js
+++ blog/src/routes/course.js
@@ -6,7 +6,9 @@ const courseController = require('../app/controllers/CourseController');
 router.get('/create', courseController.create);
 router.post('/store', courseController.store);
 router.get('/:id/edit', courseController.edit);
+router.post('/handle-form-actions', courseController.handleFormActions);
 router.put('/:id', courseController.update);
+router.patch('/:id/restore', courseController.restore); // cập nhật 1 bản ghi dùng PATCH(chuẩn restful API)
 router.patch('/:id/restore', courseController.restore); // cập nhật 1 bản ghi dùng PATCH(chuẩn restful API)
 router.delete('/:id', courseController.destroy);
 router.delete('/:id/force', courseController.forceDestroy);
