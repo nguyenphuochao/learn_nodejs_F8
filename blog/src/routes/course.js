5b1fd55842d6d6fa932cc6df1b66391cf3040e49
HaoNP <nguyenphuochao123@gmail.com>
2025-07-23 22:25:49 +0700

    remove router redundant

--- blog/src/routes/course.js
+++ blog/src/routes/course.js
@@ -9,7 +9,6 @@ router.get('/:id/edit', courseController.edit);
 router.post('/handle-form-actions', courseController.handleFormActions);
 router.put('/:id', courseController.update);
 router.patch('/:id/restore', courseController.restore); // cập nhật 1 bản ghi dùng PATCH(chuẩn restful API)
-router.patch('/:id/restore', courseController.restore); // cập nhật 1 bản ghi dùng PATCH(chuẩn restful API)
 router.delete('/:id', courseController.destroy);
 router.delete('/:id/force', courseController.forceDestroy);
 router.get('/:slug', courseController.show);
